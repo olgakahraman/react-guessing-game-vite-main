22f0e00ffb56bc7c0d2d27dc05c9c71368676788
olgakahraman <ol.khrmn@gmail.com>
2024-09-01 22:06:36 -0400

    removed .this()

--- src/GuessControl.jsx
+++ src/GuessControl.jsx
@@ -1,51 +1,6 @@
 import React, { useState } from 'react';
 import Button from './Button';
 
-// class GuessControl extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       currentGuess: "",
-//     };
-
-//     /**
-//      * These lines are required to make the methods/functions declared on this
-//      *  class have the correct `this` object when they run.
-//      */
-//     this.handleInputChange = this.handleInputChange.bind(this);
-//     this.onSubmitGuess = this.onSubmitGuess.bind(this);
-//   }
-
-//   handleInputChange(event) {
-//     this.setState({ currentGuess: event.target.value });
-//   }
-
-//   onSubmitGuess() {
-//     // Since the values from an HTML input are strings by default,
-//     //  convert to a number for the returned guess value
-//     //  by passing in the string to the Number function.
-//     // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-//     this.props.onGuess(Number(this.state.currentGuess));
-//     this.setState({ currentGuess: "" });
-//   }
-
-//   render() {
-//     return (
-// <div>
-//   <input
-//     type="number"
-//     value={this.state.currentGuess}
-//     onChange={this.handleInputChange}
-//   />
-//   <Button onClick={this.onSubmitGuess}>Submit Guess</Button>
-// </div>
-//     );
-//   }
-// }
-
-// export default GuessControl;
-
 export default function GuessControl({ onGuess }) {
 	const [currentGuess, setCurrentGuess] = useState('');
 
@@ -56,10 +11,11 @@ export default function GuessControl({ onGuess }) {
 
 	function onSubmitGuess(event) {
 		event.preventDefault();
-		onGuess(Number(this.state.currentGuess));
+		onGuess(Number(currentGuess));
 		setCurrentGuess = '';
 	}
-	return (
+  
+    	return (
 		<div>
 			<input
 				type="number"
@@ -69,4 +25,6 @@ export default function GuessControl({ onGuess }) {
 			<Button onClick={onSubmitGuess}>Submit Guess</Button>
 		</div>
 	);
+  
+
 }
